8a7d9f4cc9f990e5c90ee49ec96062174b3519fe
Emil Shirima <emil.shirima@hotmail.com>
2016-04-02 20:27:42 -0400

    Deleted the extra graph project

--- Graph/Graph/Edge.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Edge.cpp
-//  Graph
-//
-//  Created by Emil Shirima on 4/2/16.
-//  Copyright © 2016 Emil Shirima. All rights reserved.
-//
-
-#include "Edge.hpp"
--- Graph/Graph/Vertex.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Vertex.cpp
-//  Graph
-//
-//  Created by Emil Shirima on 4/2/16.
-//  Copyright © 2016 Emil Shirima. All rights reserved.
-//
-
-#include "Vertex.hpp"
